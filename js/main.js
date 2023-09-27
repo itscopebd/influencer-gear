6b790f668071725bbc666d1f3769dc9f1531b8db
itscopebd <itscopebd@gmail.com>
2023-09-27 10:59:44 -0700

    image hover effect

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let createCard = document.createElement("div");
       createCard.className = "card";
       createCard.innerHTML = `
-             <img src=${element.image} alt="" />
+             <figure><img src=${element.image} alt="" /></figure>
               <div class="product__info">
                 <h3 class="product__title">${element?.name}</h3>
                 <h4 class="price">$ ${element.price}</h4>
@@ -50,9 +50,26 @@ document.addEventListener("DOMContentLoaded", () => {
                 <p class="product__des">
                   ${element.short_description}
                 </p>
+                <button type="button" class="btn" onclick="addToCard(${element.id})" id="addToCardBtn">add to card</button>
               </div>
     `;
       showItems.appendChild(createCard);
     });
   };
 });
+
+
+
+
+
+// add to card 
+
+const addToCard=(id)=>{
+  console.log(id)
+const addToCardBtn= document.getElementById("addToCardBtn");
+const cardQuantity= document.getElementById("cardQuantity");
+addToCardBtn.disabled=true;
+console.log(addToCardBtn)
+
+
+}
