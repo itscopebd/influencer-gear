da53f342217032fa588e387f9070e88ada7f494d
itscopebd <itscopebd@gmail.com>
2023-11-04 10:56:34 -0700

    update quantity in local storage

--- js/cart.js
+++ js/cart.js
@@ -26,37 +26,31 @@ const showAddtoCartProduct = (data) => {
     let createElements = document.createElement("tr");
     createElements.innerHTML = `
       
-              <td><img style="width:150px;height:150px;border-radius:50%" src=${product.image} alt=""></td>
+              <td><img style="width:150px;height:150px" src=${product.image} alt=""></td>
               <td><h4>${product.name} </h4></td>
-              <td><input type="number" id="updateQuantity" min="1" value="1"></td>
+              <td><input type="number" onclick="updateQuantity(${product.id})" id="updateQuantity" min="1" value="1"></td>
               <td><span>$</span> ${product.price}</td>
               <td><span>$</span> ${product.price}</td>
               <td><button class="btn"><i class="fa-solid fa-trash"></i></button></td>
       
       `;
-      createCartProduct.appendChild(createElements)
+    createCartProduct.appendChild(createElements);
   });
 };
 
-//   const getShoppingCart = () => {
-//     let shoppingCart = {};
-//     const storedCart = localStorage.getItem("shopping-cart");
-//     if (storedCart) {
-//       shoppingCart = JSON.parse(storedCart);
-//     }
-//     return shoppingCart;
-//   };
 
-// add to card
 
-// const addToCard = (id) => {
-//     let shoppingCart = getShoppingCart();
-//     const quantity = shoppingCart[id];
-//     if (!quantity) {
-//       shoppingCart[id] = 1;
-//     } else {
-//       alert("This Product Already Added!");
-//     }
-//     localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
-//     quantityUpdate();
-//   };
+// update quantity in localhost 
+const updateQuantity = (id) => {
+
+  let shoppingCart = {};
+  shoppingCart = JSON.parse(localStorage.getItem("shopping-cart"));
+  const quantity = shoppingCart[id];
+  for (key in shoppingCart) {
+    if (key==id) {
+      shoppingCart[key]=quantity+1;
+    }
+  }
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart))
+  quantityUpdate()
+};
